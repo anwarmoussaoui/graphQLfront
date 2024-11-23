8e63dcb494b7f016ed98c42d6b8846251c0e15fd
anwarmoussaoui <anouar7moussaoui@gmail.com>
2024-11-23 13:44:28 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import ApolloClientProvider from './ApolloClient';
+import SoldeStats from './SoldeStats';
+import CompteList from './CompteList';
+import CompteForm from './CompteForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloClientProvider>
+      <div>
+        <h1>Bank Accounts Application</h1>
+        <SoldeStats />
+        <CompteList />
+        <CompteForm />
+      </div>
+    </ApolloClientProvider>
   );
 }
 
